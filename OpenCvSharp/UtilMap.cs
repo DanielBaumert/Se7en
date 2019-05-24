e029244ee272636d80ce05d0fb8d615dbf2d340f
Daniel Baumert <DBaumertffg@web.de>
2019-05-24 16:39:35 +0200

    Update UtilMap.cs

--- OpenCvSharp/UtilMap.cs
+++ OpenCvSharp/UtilMap.cs
@@ -1,52 +1,47 @@
-﻿using System;
-
-namespace OptTwlCtrl.OpenCvSharp
-{
-    public class UtilMap : DisposableObject
+public class UtilMap : DisposableObject
     {
-        public IntPtr DataStart { get => DataStart1(Ptr); }
+        public IntPtr DataStart { get => _DataStart(Ptr); }
         public IntPtr Ptr;
 
-        private readonly static Func<IntPtr> Create1;
-        private readonly static Func<int, int, MatType, IntPtr, ulong, IntPtr> Create2;
-        private readonly static Func<IntPtr, IntPtr> DataStart1;
-        private readonly static Action<IntPtr> DisposeUnmanaged1;
+        private readonly static Func<IntPtr> _Create;
+        private readonly static Func<int, int, MatType, IntPtr, ulong, IntPtr> _Create2;
+        private readonly static Func<IntPtr, IntPtr> _DataStart;
+        private readonly static Action<IntPtr> _DisposeUnmanaged;
 
-        public readonly static Func<IntPtr, IntPtr> InputArray;
-        public readonly static Func<IntPtr, IntPtr> OutputArray;
+        public readonly static Func<IntPtr, IntPtr> _InputArray;
+        public readonly static Func<IntPtr, IntPtr> _OutputArray;
 
         static UtilMap() {
             if (Cv2.GetCudaEnabledDeviceCount() > 0) {
-                Create1 = NativeMethods.cuda_GpuMat_new1;
-                Create2 = NativeMethods.cuda_GpuMat_new3;
-                DataStart1 = NativeMethods.cuda_GpuMat_datastart;
-                DisposeUnmanaged1 = NativeMethods.cuda_GpuMat_delete;
-                InputArray = NativeMethods.core_InputArray_new_byGpuMat;
-                OutputArray = NativeMethods.core_OutputArray_new_byGpuMat;
+                _Create = NativeMethods.cuda_GpuMat_new1;
+                _Create2 = NativeMethods.cuda_GpuMat_new3;
+                _DataStart = NativeMethods.cuda_GpuMat_datastart;
+                _DisposeUnmanaged = NativeMethods.cuda_GpuMat_delete;
+                _InputArray = NativeMethods.core_InputArray_new_byGpuMat;
+                _OutputArray = NativeMethods.core_OutputArray_new_byGpuMat;
             } else {
-                Create1 = NativeMethods.core_Mat_new1;
-                Create2 = NativeMethods.core_Mat_new8;
-                DataStart1 = NativeMethods.core_Mat_datastart;
-                DisposeUnmanaged1 = NativeMethods.cuda_GpuMat_delete;
-                InputArray = NativeMethods.core_InputArray_new_byMat;
-                OutputArray = NativeMethods.core_OutputArray_new_byMat;
+                _Create = NativeMethods.core_Mat_new1;
+                _Create2 = NativeMethods.core_Mat_new8;
+                _DataStart = NativeMethods.core_Mat_datastart;
+                _DisposeUnmanaged = NativeMethods.cuda_GpuMat_delete;
+                _InputArray = NativeMethods.core_InputArray_new_byMat;
+                _OutputArray = NativeMethods.core_OutputArray_new_byMat;
             }
         }
 
-        public UtilMap() => Ptr = Create1();
+        public UtilMap() => Ptr = _Create();
 
         /// <summary>
         /// Releases unmanaged resources
         /// </summary>
         protected override void DisposeUnmanaged() {
             if (Ptr != IntPtr.Zero) {
-                DisposeUnmanaged1(Ptr);
+                _DisposeUnmanaged(Ptr);
             }
             base.DisposeUnmanaged();
         }
 
-        public UtilMap(int rows, int cols, MatType type, IntPtr data, ulong step = 0L) => Ptr = Create2(rows, cols, type, data, step);
+        public UtilMap(int rows, int cols, MatType type, IntPtr data, ulong step = 0L) => Ptr = _Create2(rows, cols, type, data, step);
         
 
     }
-}
