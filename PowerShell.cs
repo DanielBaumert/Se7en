41b6caff30e0c8e37645284978969de43e499d59
Daniel Baumert <DBaumertffg@web.de>
2019-06-24 16:57:10 +0200

    Add files via upload

---
+++ PowerShell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+namespace WindowsClean {
+    public static class PowerShell {
+        private static ProcessStartInfo ProcessStartInfo = new ProcessStartInfo() {
+            FileName = @"C:\windows\system32\windowspowershell\v1.0\powershell.exe",
+            CreateNoWindow = true,
+            ErrorDialog = false,
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            WindowStyle = ProcessWindowStyle.Hidden
+        };
+
+
+        public static string RunCommand(string argument) {
+            if (argument.Contains(@""""))
+                throw new NotSupportedException(@"use ' instat of """);
+
+            ProcessStartInfo.Arguments = argument;
+            using(Process proc = new Process() { StartInfo = ProcessStartInfo }) {
+                proc.Start();
+                while(!proc.StandardOutput.EndOfStream) {
+                    string result = proc.StandardOutput.ReadToEnd();
+                    return result;
+                }
+                return string.Empty;
+            }
+        }
+    }
+}
